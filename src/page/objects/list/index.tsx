4f835e5f3e63d1ba3535e92e210503d705dac157
demetrio.lopes <demetrio.lopes@conecthus.org.br>
2026-03-16 17:33:39 -0400

    Adding MUI table

--- src/page/objects/list/index.tsx
+++ src/page/objects/list/index.tsx
@@ -1,22 +1,43 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useListObject } from "./useListObject";
+import { TableContainer, Paper } from "@mui/material";
 
+export function ListObject() {
+  const { objectList } = useListObject();
 
-import { useListObject } from "./useListObject"
-
-
-
-export function ListObject (){
-    const {objectList} = useListObject()
-
-    return(
-   <>
-    {
-       <ul>
-       {objectList?.map((item) => (
-         <li key={item.id}>{item.name}</li>
-       ))}
-     </ul>
-    }
-   
-   </>
-    )
+  const firstItem = objectList?.[0];
+  return (
+    <TableContainer component={Paper} sx={{ maxWidth: 600, margin: "0 auto" }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {firstItem &&
+              ["id", "name", ...Object.keys(firstItem.data ?? {})].map(
+                (key) => <TableCell key={key}>{key}</TableCell>
+              )}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {objectList?.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>
+                {String(item.data?.color ? item.data?.color : "-")}
+              </TableCell>
+              <TableCell>
+                {String(item.data?.capacity ? item.data?.capacity : "-")}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
