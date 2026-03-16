5da26758dd19e38379a7fe5abf4c25fb22b25c37
demetrio.lopes <demetrio.lopes@conecthus.org.br>
2026-03-16 14:48:21 -0400

    typing useQuery responsible for listing

--- src/page/objects/list/useListObject.tsx
+++ src/page/objects/list/useListObject.tsx
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { useObject } from "../../../hook/useObject";
+import type { ObjectResponseList } from "../../../type/object.response.shema";
 
 export function useListObject(){
     const {getAll} = useObject();
 
-    const {data: objectList} = useQuery({
+    const {data: objectList} = useQuery<ObjectResponseList>({
         queryKey: ["objectList"],
         queryFn:getAll,
         staleTime: Infinity,
     })
-
 
     return{
         objectList
