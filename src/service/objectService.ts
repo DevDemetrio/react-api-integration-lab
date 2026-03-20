c3f8694ff359dbf48be6471641dc278ba1b11da0
demetrio.lopes <demetrio.lopes@conecthus.org.br>
2026-03-20 08:41:34 -0400

    Adding to the service to add get object by id

--- src/service/objectService.ts
+++ src/service/objectService.ts
@@ -1,7 +1,7 @@
 
 import ApiClient from "../api/apiClient";
 import { ObjectPayloadShema, type ObjectPayloadType } from "../type/object.payload.schema";
-import { ObjectResponseListShema} from "../type/object.response.shema";
+import { ObjectResponseListShema, ObjectResponseShema} from "../type/object.response.shema";
 
 
 export class PostService extends ApiClient {
@@ -13,6 +13,11 @@ export class PostService extends ApiClient {
     async postObject(payload: ObjectPayloadType){
         const res = await this.api.post("/objects", payload);
         return ObjectPayloadShema.parse(res.data)
+    }
+
+    async objectByid(id: string){
+        const res = await this.api.get(`/objects/${id}`)
+        return ObjectResponseShema.parse(res.data)
     }
 }
 
